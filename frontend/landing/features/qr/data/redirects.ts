7de72f66f01d45a594be69f0e9eccaa7c5f5261f
ARKCYGNUS\Cygnus <keventandika@gmail.com>
2026-08-07 15:02:45 +0700

    frontend - adding qr slug for bumbu instant

--- frontend/landing/features/qr/data/redirects.ts
+++ frontend/landing/features/qr/data/redirects.ts
@@ -22,8 +22,50 @@ export const redirects: QRRedirect[] = [
     },
     {
         id: "QR-0004",
-        slug: "tongkol-cabai-hijau",
+        slug: "ayam-fillet-bakar-manis",
         destination: LINKS.internal.home,
         active: false,
+    },
+    {
+        id: "QR-0005",
+        slug: "bumbu-nasi-goreng-kencur",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0006",
+        slug: "bumbu-banjar",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0007",
+        slug: "bumbu-woku",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0008",
+        slug: "bumbu-manis",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0009",
+        slug: "bumbu-rica",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0010",
+        slug: "bumbu-kentang-balado",
+        destination: LINKS.internal.home,
+        active: true,
+    },
+    {
+        id: "QR-0011",
+        slug: "bumbu-lado-hijau",
+        destination: LINKS.internal.home,
+        active: true,
     },
 ];
